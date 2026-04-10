8b67daf30d9b6ae439677f8c13ecb34256978d4f
Mohamed Sabry <Mohamed.sabry.av@gmail.com>
2026-04-10 06:55:53 +0200

    initial commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,64 @@
+import { appRouter } from "@focusUp/api/routers/index";
+import { env } from "@focusUp/env/server";
+import { OpenAPIHandler } from "@orpc/openapi/node";
+import { OpenAPIReferencePlugin } from "@orpc/openapi/plugins";
+import { onError } from "@orpc/server";
+import { RPCHandler } from "@orpc/server/node";
+import { ZodToJsonSchemaConverter } from "@orpc/zod/zod4";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: env.CORS_ORIGIN,
+    methods: ["GET", "POST", "OPTIONS"],
+  }),
+);
+
+const rpcHandler = new RPCHandler(appRouter, {
+  interceptors: [
+    onError((error) => {
+      console.error(error);
+    }),
+  ],
+});
+const apiHandler = new OpenAPIHandler(appRouter, {
+  plugins: [
+    new OpenAPIReferencePlugin({
+      schemaConverters: [new ZodToJsonSchemaConverter()],
+    }),
+  ],
+  interceptors: [
+    onError((error) => {
+      console.error(error);
+    }),
+  ],
+});
+
+app.use(async (req, res, next) => {
+  const rpcResult = await rpcHandler.handle(req, res, {
+    prefix: "/rpc",
+    context: {},
+  });
+  if (rpcResult.matched) return;
+
+  const apiResult = await apiHandler.handle(req, res, {
+    prefix: "/api-reference",
+    context: {},
+  });
+  if (apiResult.matched) return;
+
+  next();
+});
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@focusUp/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
